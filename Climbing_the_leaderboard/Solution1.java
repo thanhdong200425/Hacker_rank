95a80247bf8d5ec310dcd35300c62383635088c6
Dong <thanhdong200425@gmail.com>
2023-08-20 19:04:53 +0700

    .

---
+++ Climbing_the_leaderboard/Solution1.java
@@ -0,0 +1,42 @@
+package Climbing_the_leaderboard;
+
+import java.util.*;
+
+class Solution {
+
+    private static Map<Integer, Integer> removeDuplicate(List<Integer> ranked) {
+        Map<Integer, Integer> scoreToRank = new HashMap<Integer, Integer>();
+        int currentRank = 1;
+        for (int i = 0; i < ranked.size(); i++) {
+            int score = ranked.get(i);
+            if (!scoreToRank.containsKey(score)) {
+                scoreToRank.put(score, currentRank);
+                currentRank++;
+            }
+        }
+        return scoreToRank;
+    }
+
+    public static List<Integer> climbingLeaderboard(List<Integer> ranked, List<Integer> players) {
+        if (ranked.isEmpty() || players.isEmpty()) {
+            return null;
+        }
+
+        List<Integer> result = new ArrayList<Integer>();
+        Map<Integer, Integer> scoreToRank = removeDuplicate(ranked);
+
+        int index = ranked.size() - 1;
+        for (int i = 0; i < players.size(); i++) {
+            int playerScore = players.get(i);
+            while (index >= 0 && playerScore >= ranked.get(index)) {
+                index--;
+            }
+
+            int playerRank = (index < 0) ? 1 : (scoreToRank.get(ranked.get(index)) + 1);
+            result.add(playerRank);
+        }
+
+        return result;
+    }
+
+}
---
+++ Climbing_the_leaderboard/Solution2.java
@@ -0,0 +1,63 @@
+package Climbing_the_leaderboard;
+
+import java.util.*;
+
+public class Solution2 {
+    private static List<Integer> combineArray(List<Integer> a, List<Integer> b) {
+        a.addAll(b);
+        return a;
+    }
+
+    private static List<Integer> removeDuplicate(List<Integer> a) {
+        Set<Integer> set = new HashSet<Integer>();
+        for (int i : a) {
+            if (!set.contains(i)) {
+                set.add(i);
+            }
+        }
+        a.removeAll(a);
+        a.addAll(set);
+        return a;
+    }
+
+    private static Map<Integer, Integer> createMap(List<Integer> a) {
+        Map<Integer, Integer> map = new HashMap<>();
+        Collections.sort(a, Collections.reverseOrder());
+        int currentRank = 1, length = a.size();
+        for (int i = 0; i < length; i++) {
+            int score = a.get(i);
+            if (!map.containsKey(score)) {
+                map.put(score, currentRank++);
+            }
+        }
+        return map;
+    }
+
+    public static List<Integer> climbingLeaderboard(List<Integer> ranked, List<Integer> players) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> ranked = new ArrayList<>(Arrays.asList(100, 90, 80, 80, 40));
+        List<Integer> players = new ArrayList<>(Arrays.asList(70, 80, 120));
+
+        List<Integer> combineArray = Solution2.combineArray(ranked, players);
+        combineArray = Solution2.removeDuplicate(combineArray);
+        Map<Integer, Integer> map = Solution2.createMap(combineArray);
+
+        List<Integer> result = new ArrayList<>();
+        int i = 0;
+
+        for (int j = players.size() - 1; j >= 0; j--) {
+            Integer playerScore = players.get(j) - i + 1;
+            if (map.containsKey(playerScore)) {
+                result.add(map.get(playerScore));
+            }
+            i++;
+        }
+
+        for (int z : result) {
+            System.out.println(z);
+        }
+    }
+}
